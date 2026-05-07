30b86a7baee8e6c67eb8d545cb21ac5e59c9f5a8
DipenShrestha7 <sdipen561@gmail.com>
2026-05-07 19:40:53 +0545

    used crawl4ai to get financial data and store it in the database and a json file

---
+++ Backend/financial2/try.py
@@ -0,0 +1,11 @@
+import os,json
+
+base_dir = os.path.dirname(__file__)
+parent_dir = os.path.dirname(base_dir)
+
+file_path = os.path.join(parent_dir, "save_data", "companies2.json")
+
+with open(file_path, "r") as f:
+    scrips = json.load(f)
+
+print(scrips[9:19])
